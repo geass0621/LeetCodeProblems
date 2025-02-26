85dd402e8873713e9d2fcb086a0b179829348ddd
DongCuong <geas0621@gmail.com>
2025-02-26 21:08:49 +0200

    Solved: remove elements

--- Binary Search.js
+++ Binary Search.js
@@ -29,26 +29,50 @@
  * @param {number} target
  * @return {number}
  */
+// var search = function (nums, target) {
+
+//   let left = 0;
+//   let right = nums.length - 1;
+
+//   while (left <= right) {
+//     let mid = Math.floor((left + right) / 2);
+
+//     if (nums[mid] === target) {
+//       return mid;
+//     } else if (nums[mid] < target) {
+//       left = mid + 1;
+//     } else {
+//       right = mid - 1;
+//     }
+//   }
+
+//   return -1;
+
+// };
+
+
 var search = function (nums, target) {
 
-  let left = 0;
-  let right = nums.length - 1;
+  let recursiveSearch = (nums, target, left, right) => {
+    if (left > right) {
+      return -1;
+    }
 
-  while (left <= right) {
     let mid = Math.floor((left + right) / 2);
 
     if (nums[mid] === target) {
       return mid;
     } else if (nums[mid] < target) {
-      left = mid + 1;
+      return recursiveSearch(nums, target, mid + 1, right);
     } else {
-      right = mid - 1;
+      return recursiveSearch(nums, target, left, mid - 1);
     }
-  }
+  };
 
-  return -1;
+  return recursiveSearch(nums, target, 0, nums.length - 1);
 
 };
+
 
 let nums = [-1, 0, 3, 5, 9, 12];
 let target = 9;
